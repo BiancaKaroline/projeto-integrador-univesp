44bf63f2c124dfb13e1f16ba19b482845607970e
Bianca Karoline <66279544+BiancaKaroline@users.noreply.github.com>
2021-10-17 14:25:01 -0300

    Initial commit

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
+export default class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+}
